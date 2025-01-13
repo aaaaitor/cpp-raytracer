ec58149ad910639007df24a2a2809f7d5a5bc2e0
aaaaitor_ <61393970+aaaaitor@users.noreply.github.com>
2025-01-13 19:24:23 +0100

    Add vector definitions and operators.

---
+++ geometry.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#ifndef __GEOMETRY_H__
+
+#define __GEOMETRY_H__
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <iostream>
+
+//---------------------------------------
+// DEFINITIONS FOR VECTOR VARIABLE TYPES
+//---------------------------------------
+
+//Vector definition
+template <size_t DIM, typename T> struct vec {
+	vec() { for (size_t i = DIM; i--; data_[i] = T()); }
+	T& operator[](const size_t i) { assert(i < DIM); return data_[i]; }
+	const T& operator[](const size_t i) const { assert(i < DIM); return data_[i]; }
+private:
+	T data_[DIM];
+};
+
+typedef vec<2, float>	Vec2f;	//Vector 2 float definition
+typedef vec<3, float>	Vec3f;	//Vector 3 float definition
+typedef vec<3, int>		Vec3i;	//Vector 3 integer definition
+typedef vec<4, float>	Vec4f;	//Vector 4 float definition
+
+template <typename T> struct vec<2, T> {
+	vec() : x(T()), y(T()) {}
+	vec(T X, T Y) : x(X), y(Y) {}
+	template <class U> vec<2, T>(const vec<2, U>& v);
+	T& operator[](const size_t i) { assert(i < 2); return i <= 0 ? x : y; }
+	const T& operator[](const size_t i) const { assert(i < 2); return i <= 0 ? x : y; }
+};
+
+template <typename T> struct vec<3, T> {
+	vec() : x(T()), y(T()), z(T()) {}
+	vec(T X, T Y, T Z) : x(X), y(Y), z(Z) {}
+	T& operator[](const size_t i) { assert(i < 3); return i <= 0 ? x : (1 == i ? y : z); }
+	const T& operator[](const size_t i) const { assert(i < 3); return i <= 0 ? x : (1 == i ? y : z); }
+	float norm() { return std::sqrt(x * x + y * y + z * z); }
+	vec<3, T>& normalize(T l = 1) { *this = (*this) * (l / norm()); return *this; }
+	T x, y, z;
+};
+
+template <typename T> struct vec<4, T> {
+	vec() : x(T()), y(T()), z(T()), w(T()) {}
+	vec(T X, T Y, T Z, T W) : x(X), y(Y), z(Z), w(W) {}
+	T& operator[](const size_t i) { assert(i < 4); return i <= 0 ? x : (1 == i ? y : (2 == i ? z : w)); }
+	const T& operator[](const size_t i) const { assert(i < 4); return i <= 0 ? x : (1 == i ? y : (2 == i ? z : w)); }
+	T x, y, z, w;
+};
+
+//----------------------------------------
+// DEFINITIONS MATH OPERATORS FOR VECTORS
+//----------------------------------------
+
+template<size_t DIM, typename T> T operator*(const vec<DIM, T>& lhs, const vec<DIM, T>& rhs) {						//Vector multiplication
+	T ret = T();
+	for (size_t i = DIM; i--; ret += lhs[i] * rhs[i]);
+	return ret;
+}
+
+template<size_t DIM, typename T> vec<DIM, T> operator+(vec<DIM, T> lhs, const vec<DIM, T>& rhs) {					//Vector addition
+	for (size_t i = DIM; i--; lhs[i] += rhs[i]);
+	return lhs;
+}
+
+template<size_t DIM, typename T> vec<DIM, T> operator-(vec<DIM, T> lhs, const vec<DIM, T>& rhs) {					//Vector substraction
+	for (size_t i = DIM; i--; lhs[i] -= rhs[i]);
+	return lhs;
+}
+
+template<size_t DIM, typename T, typename U> vec<DIM, T> operator*(const vec<DIM, T>& lhs, const U& rhs) {			//Vector multiplication by scalar value
+	vec<DIM, T> ret;
+	for (size_t i = DIM; i--; ret[i] = lhs[i] * rhs);
+	return ret;
+}
+
+template<size_t DIM, typename T> vec<DIM, T> operator-(const vec<DIM, T>& lhs) {									//Vector unary negation
+	return lhs * T(-1);
+}
+
+template <typename T> vec<3, T> cross(vec<3, T> v1, vec<3, T> v2) {													//Vector cross product
+	return vec<3, T>(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
+}
+
+template <size_t DIM, typename T> std::ostream& operator<<(std::ostream& out, const vec<DIM, T>& v) {				//Vector left shift operator
+	for (unsigned int i = 0; i < DIM; i++) {
+		out << v[i] << " ";
+	}
+	return out;
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <limits>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "geometry.h"
+
+void render() {
+
+}
