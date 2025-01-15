ea360bbe6e81b9198e2b9a38933704fbe384cd25
aaaaitor_ <61393970+aaaaitor@users.noreply.github.com>
2025-01-15 23:37:32 +0100

    Enviroment map ray-tracing.

--- renderer.cpp
+++ renderer.cpp
@@ -1,5 +1,6 @@
 #define _USE_MATH_DEFINES
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
 #include <limits>
 #include <cmath>
 #include <iostream>
@@ -7,6 +8,10 @@
 #include <vector>
 #include "geometry.h"
 #include "stb_image_write.h"
+#include "stb_image.h"
+
+int envmap_width, envmap_height;
+std::vector<Vec3f> envmap;
 
 //Define a light source
 struct Light {
@@ -151,12 +156,29 @@ Vec3f cast_ray_specular(const Vec3f& orig, const Vec3f& dir, const std::vector<S
     return Vec3f(0,0,0) * diffuse_light_intensity * material.albedo[0] + Vec3f(1., 1., 1.) * specular_light_intensity * material.albedo[1];
 }
 
+Vec3f get_environment_color(const Vec3f& ray_direction) {
+    
+    float theta = acos(ray_direction.y);
+    float phi = atan2(ray_direction.z, ray_direction.x);
+
+    float u = (phi + M_PI) / (2 * M_PI);
+    float v = (theta) / M_PI;
+
+    int x = int(u * envmap_width);
+    int y = int(v * envmap_height);
+
+    x = std::min(std::max(x, 0), envmap_width - 1);
+    y = std::min(std::max(y, 0), envmap_height - 1);
+
+    return envmap[x + y * envmap_width];
+}
+
 Vec3f cast_ray_final(const Vec3f& orig, const Vec3f& dir, const std::vector<Sphere>& spheres, const std::vector<Light>& lights, size_t depth = 0) {
     Vec3f point, N;
     Material material;
 
     if (depth > max_depth || !scene_intersect(orig, dir, spheres, point, N, material)) {
-        return background_color;
+        return get_environment_color(dir);
     }
 
     Vec3f reflect_dir = reflect(dir, N).normalize();
@@ -322,8 +344,7 @@ void render_specular(const std::vector<Sphere>& spheres, const std::vector<Light
 }
 
 void render_final(const std::vector<Sphere>& spheres, const std::vector<Light>& lights) {
-    std::vector<Vec3f> framebuffer(width * height);                             //Pixel buffer
-    unsigned char* data = (unsigned char*)malloc(width * height * channels);    //Image data allocation
+    std::vector<Vec3f> framebuffer(width * height);     //Pixel buffer
 
     //Image generation
     #pragma omp parallel for
@@ -339,25 +360,46 @@ void render_final(const std::vector<Sphere>& spheres, const std::vector<Light>& 
         }
     }
 
+    //Enviroment pixmap
+    std::vector<unsigned char> pixmap(width*height*3);
     //Save image
     for (size_t i = 0; i < height * width; ++i) {
         Vec3f& c = framebuffer[i];
         float max = std::max(c[0], std::max(c[1], c[2]));
         if (max > 1) c = c * (1. / max);
         for (size_t j = 0; j < 3; j++) {
-            assert(data != nullptr);
-            data[i * 3 + j] = (unsigned char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
+            pixmap[i * 3 + j] = (unsigned char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
         }
     }
 
     //Save image
-    stbi_write_jpg("./output/4_final.jpg", width, height, channels, data, quality);
-    free(data);
+    stbi_write_jpg("./output/4_final.jpg", width, height, channels, pixmap.data(), quality);
 }
 
 int main() {
     std::vector<Sphere> spheres;
     std::vector<Light>  lights;
+
+    std::cout << "[INFO/Resources/Images] Loading enviroment image." << std::endl;
+
+    //Load and read enviroment .jpg
+    int n = -1;
+    unsigned char* pixmap = stbi_load("./resources/envmap.jpg", &envmap_width, &envmap_height, &n, 0);
+    if (!pixmap || 3 != n) {
+        std::cerr << "[Error] Can not load the environment map." << std::endl;
+        return -1;
+    }
+    envmap = std::vector<Vec3f>(envmap_width * envmap_height);
+    for (int j = envmap_height - 1; j >= 0; j--) {
+        for (int i = 0; i < envmap_width; i++) {
+            envmap[i + j * envmap_width] = Vec3f(pixmap[(i + j * envmap_width) * 3 + 0], pixmap[(i + j * envmap_width) * 3 + 1], pixmap[(i + j * envmap_width) * 3 + 2]) * (1 / 255.);
+        }
+    }
+    stbi_image_free(pixmap);
+
+    std::cout << "[INFO/Resources/Images] Enviroment image loaded." << std::endl;
+
+    std::cout << "[INFO/Resources/Materials] Defining materials." << std::endl;
 
     //Define materials
     Material emerald_green(1.0, Vec4f(0.6,  0.6, 0.6, 0.0),  Vec3f(0.1, 0.7, 0.1),    70.);
@@ -366,6 +408,10 @@ int main() {
     Material        mirror(1.0, Vec4f(0.0, 10.0, 0.8, 0.0),  Vec3f(1.0, 1.0, 1.0),  1425.);
     Material         glass(1.5, Vec4f(0.0,  0.5, 0.1, 0.8),  Vec3f(0.6, 0.7, 0.8),   125.);
 
+    std::cout << "[INFO/Resources/Materials] Materials defined." << std::endl;
+
+    std::cout << "[INFO/Scene/Meshes] Creating meshes." << std::endl;
+
     //Add spheres
     spheres.push_back(Sphere(Vec3f(  -3,    0, -16), 2,  emerald_green));
     spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2,          glass));
@@ -373,23 +419,43 @@ int main() {
     spheres.push_back(Sphere(Vec3f(   7,    5, -18), 4,         mirror));
     spheres.push_back(Sphere(Vec3f(  -7, -1.5, -12), 2,   marble_white));
 
+    std::cout << "[INFO/Scene/Meshes] Meshes created." << std::endl;
+
+    std::cout << "[INFO/Scene/Lighting] Creating lights." << std::endl;
+
     //Add light sources
     lights.push_back(Light(Vec3f(-20, 20, 20), 1.5));
     lights.push_back(Light(Vec3f(30, 50, -25), 1.8));
     lights.push_back(Light( Vec3f(30, 20, 30), 1.7));
 
+    std::cout << "[INFO/Scene/Lighting] Lights created." << std::endl;
 
     ////Render base image
     //render_base();
     
+    std::cout << "[INFO/Rendering] Starting rendering queue." << std::endl;
+
+    std::cout << "[INFO/Rendering/Ambient] Starting ambient image render." << std::endl;
     //Render ambient image
     render_ambient(spheres);
+    std::cout << "[INFO/Rendering/Ambient] Finished ambient image render." << std::endl;
+
+    std::cout << "[INFO/Rendering/Diffuse] Starting diffuse image render." << std::endl;
     //Render diffuse image
     render_diffuse(spheres, lights);
+    std::cout << "[INFO/Rendering/Diffuse] Finished diffuse image render." << std::endl;
+
+    std::cout << "[INFO/Rendering/Specular] Starting specular image render." << std::endl;
     //Render specular
     render_specular(spheres, lights);
+    std::cout << "[INFO/Rendering/Specular] Finished specular image render." << std::endl;
+
+    std::cout << "[INFO/Rendering/Specular] Starting final image render." << std::endl;
     //Render final
     render_final(spheres, lights);
+    std::cout << "[INFO/Rendering/Specular] Finished final image render." << std::endl;
+
+    std::cout << "[INFO/Rendering] Rendering queue finished." << std::endl;
 
     return 0;
 }
